607899820d5589f5336db02d3bf3e7f61fe87d3b
zakari-aee <zakariaealliouate@gmail.com>
2026-09-11 22:46:00 +0100

    Add initial implementation of libft functions and Makefile => day 1

---
+++ Makefile
@@ -0,0 +1,6 @@
+NAME = libft.a
+CC = gcc
+FLAGS = -Wall -Wextra -Werror
+
+SRCS = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isprint.c ft_strlen.c ft_memset.c
+
---
+++ ft_memset.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+    unsigned char *z;
+    size_t i;
+
+    z = (unsigned char *)b;
+    i = 0;
+    while(i < len)
+    {
+        z[i] = (unsigned char)c;
+        i++;
+    }
+    return b;
+
+}
